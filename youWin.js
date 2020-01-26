786ffa5dc56817024de0578e1dd36dbed1fc873a
ConnorShultz <55148193+ConnorShultz@users.noreply.github.com>
2020-01-26 16:18:31 -0500

    Update youWin.js
    Final code for youWin.js

--- youWin.js
+++ youWin.js
@@ -7,6 +7,6 @@ function Win() {
     text("YOU WIN!", 125, 100);
     text("You Scored 5 Goals!!!", 10, 200);
     textSize(20);
-    text("Press play (top left of screen) to play again", 60, 370);
+    text("GAME OVER", 190, 370);
   }
 }
